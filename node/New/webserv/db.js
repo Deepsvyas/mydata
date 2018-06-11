1c2864ef5e5a261fe2cd8791e63147f3155e5e1f
Dilip Vyas <dilip@cibirix.com>
2018-06-11 18:33:13 +0530

    changes

--- node/New/webserv/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(callback) {
-    var MongoClient = require( 'mongodb' ).MongoClient;
-    var url = 'mongodb://127.0.0.1:27017/mydb';
-    MongoClient.connect(url, callback);
-}
-
-
